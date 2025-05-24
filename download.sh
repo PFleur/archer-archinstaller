bf9d180ebad982e778487b821449944af62823ef
PFleur <106462865+PFleur@users.noreply.github.com>
2025-05-24 15:39:37 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -5,9 +5,9 @@ branch="main"
 
 mkdir ./archer
 
-curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/0-0_archLiveEnvironment.sh > ./archr/0-0_archLiveEnvironment.sh
-curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/0-1_rootInitialSetup.sh > ./archr/0-1_rootInitialSetup.sh
-curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/1_gitPrograms.sh > ./archr/1_gitPrograms.sh
-curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/2_xorgSetup.sh > ./archr/2_xorgSetup.sh
-curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/3_pacmanPackages.sh > ./archr/3_pacmanPackages.sh
-curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/4_aur.sh > ./archr/4_aur.sh
+curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/0-0_archLiveEnvironment.sh > ./archer/0-0_archLiveEnvironment.sh
+curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/0-1_rootInitialSetup.sh > ./archer/0-1_rootInitialSetup.sh
+curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/1_gitPrograms.sh > ./archer/1_gitPrograms.sh
+curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/2_xorgSetup.sh > ./archer/2_xorgSetup.sh
+curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/3_pacmanPackages.sh > ./archer/3_pacmanPackages.sh
+curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/4_aur.sh > ./archer/4_aur.sh
