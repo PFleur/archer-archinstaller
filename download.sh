848e0498c72b904bcd84c3aeb98e97ab9bea1dd5
PFleur <106462865+PFleur@users.noreply.github.com>
2025-05-24 15:36:23 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,3 +2,10 @@
 
 gitUser="PFleur"
 branch="main"
+
+curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/0-0_archLiveEnvironment.sh > ./archr/0-0_archLiveEnvironment.sh
+curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/0-1_rootInitialSetup.sh > ./archr/0-1_rootInitialSetup.sh
+curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/1_gitPrograms.sh > ./archr/1_gitPrograms.sh
+curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/2_xorgSetup.sh > ./archr/2_xorgSetup.sh
+curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/3_pacmanPackages.sh > ./archr/3_pacmanPackages.sh
+curl -sL https://raw.githubusercontent.com/PFleur/archer-archinstaller/main/4_aur.sh > ./archr/4_aur.sh
